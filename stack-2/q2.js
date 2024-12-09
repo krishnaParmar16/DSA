b4844db1bea44ce03a44a7e47f55b1a73651f717
krishnaParmar16 <kp497485@gmail.com>
2024-12-09 23:48:32 +0530

    code

--- stack-2/q2.js
+++ stack-2/q2.js
@@ -1,1 +1,27 @@
 // Question 2 :  Given an array [4, 5, 2, 25], find the next greater element for each element.
+
+function next()
+{
+    let arr=[4,5,2,25];
+    let newarr=Array(arr.length).fill(-1)
+    let stack=[];
+
+    for(let i=0;i<arr.length;i++)
+    {
+        stack.push(i);
+
+        if(arr[i+1] >arr[i])
+        {
+            // console.log(arr[i+1]);
+            
+            while(stack.length>0)
+            {
+                let great=stack[stack.length-1];
+                newarr[great]=arr[i+1];
+                stack.pop()
+            }
+        }
+    }
+    return newarr;
+}
+console.log(next());
