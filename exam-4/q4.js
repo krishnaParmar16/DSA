ae79cc7d05916403d91a5dfce583506848b51a27
krishnaParmar16 <kp497485@gmail.com>
2024-12-10 23:50:46 +0530

    code

--- exam-4/q4.js
+++ exam-4/q4.js
@@ -1,23 +1,56 @@
 // ### Q4: Count Nodes
 // Count the total number of nodes in a linked list.
 // **Example:**
-// ```
 // Input:
 // 1 2 3 4 5 -1
 // Output:
 // 5
-// ```
-function count()
-{
-    let arr=[1, 2, 3, 4, 5, -1];
-    let count=0;
-    for(let i=0;i<arr.length;i++)
-    {
-        if(arr[i]>=0)
-        {
-            count++;
-        }
+
+
+// function count()
+// {
+//     let arr=[1, 2, 3, 4, 5, -1];
+//     let count=0;
+//     for(let i=0;i<arr.length;i++)
+//     {
+//         if(arr[i]>=0)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// console.log(count());
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
     }
+}
+
+
+function countNodes(head) {
+    let count = 0;
+    let current = head;
+
+    
+    while (current !== null) {
+        count++;
+        current = current.next; 
+    }
+
     return count;
 }
-console.log(count());
+
+
+let head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+head.next.next.next.next.new= new Node(-1);
+
+
+
+console.log(countNodes(head))
--- exam-4/q8.js
+++ exam-4/q8.js
@@ -6,3 +6,32 @@
 // A man, a plan, a canal: Panama
 // Output:
 // true
+
+function ispalindrome(str)
+{
+    let newstr="";
+
+    for(let char of str)
+    {
+        if((char>="a" && char <="z") || (char>="A" && char <="Z") || (char>="0" && char <="9"))
+        {
+            newstr=newstr+char.toLowerCase();
+        }
+    }
+
+    let reversestr="";
+    for(let i=newstr.length-1;i>=0;i--)
+    {
+        reversestr=reversestr+newstr[i];
+    }
+    if(reversestr === newstr)
+    {
+        return true;
+    }
+    else{
+        return false
+    }
+
+}
+let str="A man, a plan, a canal: Panama";
+console.log(ispalindrome(str));
